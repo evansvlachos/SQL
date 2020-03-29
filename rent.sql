28422bc701032bc9d6de6e21c66b5a9b1a70183e
evansvlachos <49921233+evansvlachos@users.noreply.github.com>
2020-03-29 12:39:56 +0300

    Update rent.sql

--- rent.sql
+++ rent.sql
@@ -1,3 +1,10 @@
-create databasse rent:
-create table rent:
-
+create database rent:
+create table ireland:
+id int:
+name text(12):
+adress text(12);
+num text(12);
+insert into rent(id,name,adress,num):
+values(1,killardy,dublin,45)
+insert into rent(id,name,adress,num):
+values(1,killardy,dublin,45)
