07587296e6907708e2ae9ebaf82037bbe4cffccb
evansvlachos <49921233+evansvlachos@users.noreply.github.com>
2020-05-16 17:04:55 +0300

    Update rent.sql

--- rent.sql
+++ rent.sql
@@ -1,3 +1,4 @@
+/*create the table rent*/
 create database rent:
 create table ireland:
 id in
@@ -5,10 +6,11 @@ name text(12):
 adress text(12);
 num text(12);
 insert into rent(id,name,adress,num):
-values(1,killardy,dublin,45);
+values(1,killardy,dublin,45);/*data on first line*/
 insert into rent(id,name,adress,num):
 values(2,Gallery Quay, Grand Canal Quay,dublin,45);
-create coloum 
+ALTER TABLE rent
+ADD column_price text; 
 /*1. Gallery Quay, Grand Canal Quay, Dublin Docklands, Dublin 2  €2,100 monthly https://www.rent.ie/houses-to-let/Gallery-Quay-Grand-Canal-Quay-Dublin-Docklands-Dublin-2-Dublin-City-Centre/2030289/ */
-/*https://www.rent.ie/houses-to-let/The-Old-Music-Hall-Cows-Lane-Temple-Bar-Dublin-2-Dublin-City-Centre/2030256/ */
+/*https://www.rent.ie/houses-to-let/The-Old-Music-Hall-Cows-Lane-Temple-Bar-Dublin-2-Dublin-City-Centre/2030256/ 1965 */
 where id=1 insert into price=2500
