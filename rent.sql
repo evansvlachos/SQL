b73ed0f9c02912fb43707008ac51aced17d9c29d
evansvlachos <49921233+evansvlachos@users.noreply.github.com>
2020-06-12 23:04:03 +0300

    Update rent.sql

--- rent.sql
+++ rent.sql
@@ -10,7 +10,7 @@ values(1,killardy,dublin,45);/*data on first line*/
 insert into rent(id,name,adress,num):
 values(2,Gallery Quay, Grand Canal Quay,dublin,45);
 ALTER TABLE rent
-ADD column_price text; 
+ADD column_price boolean; 
 /*1. Gallery Quay, Grand Canal Quay, Dublin Docklands, Dublin 2  €2,100 monthly https://www.rent.ie/houses-to-let/Gallery-Quay-Grand-Canal-Quay-Dublin-Docklands-Dublin-2-Dublin-City-Centre/2030289/ */
 /*https://www.rent.ie/houses-to-let/The-Old-Music-Hall-Cows-Lane-Temple-Bar-Dublin-2-Dublin-City-Centre/2030256/ 1965 */
 UPDATE ireland
