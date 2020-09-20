1683099a5a77d396642ffbb24fa3d249db626e3d
vangelis <49921233+evansvlachos@users.noreply.github.com>
2020-09-20 18:06:42 +0300

    Update party.sql

--- party.sql
+++ party.sql
@@ -7,7 +7,7 @@ create coloum surname=string;
 create coloum telephone=boulean;
 create coloum email=string;
 create coloum town=string;
- 
+
 create table filoi;
 create coloum id=boolean;
 create coloum name=string;
@@ -16,3 +16,6 @@ create coloum telephone;
 create coloum email=string=;
 create coloum town=string;
 create  
+
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);  
