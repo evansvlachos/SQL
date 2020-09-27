0293b4145947a62d79f55d966a2075de63fd2afd
vangelis <49921233+evansvlachos@users.noreply.github.com>
2020-09-27 06:28:43 +0300

    Update party.sql

--- party.sql
+++ party.sql
@@ -13,9 +13,9 @@ create coloum id=boolean;
 create coloum name=string;
 create coloum surname=string;
 create coloum telephone;
-create coloum email=string=;
-create coloum town=string;
-create  
+create coloum email=string;
+create coloum town=string; 
 
+/*
 INSERT INTO table_name (column1, column2, column3, ...)
 VALUES (value1, value2, value3, ...);  
